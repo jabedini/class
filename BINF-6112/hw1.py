422f30613400edc203cbedbf8d811411016c919f
jabedini <jabedini@uncc.edu>
2017-02-02 08:29:38 -0500

    Create hw1.py

---
+++ BINF-6112/hw1.py
@@ -0,0 +1,70 @@
+
+class Life:
+    def display(self)
+
+class Bacteria(Life):
+    def display(self)
+
+class Thermotoga(Bacteria):
+    def display(self)
+
+class Bacteroide(Bacteria):
+    def display(self)
+
+class Cyanobacteria(Bacteria):
+    def display(self)
+
+class Purple_Bacteria(Bacteria):
+    def display(self)
+
+class Gram_positives(Bacteria):
+    def display(self)
+
+class Green_nonsulfur_bacteria(Bacteria):
+    def display(self)
+
+class Archaea(Life):
+    def display(self)
+
+class Pyrodictium(Archaea):
+    def display(self)
+
+class Thermoproteus(Archaea):
+    def display(self)
+
+class Thermococcales(Archaea):
+    def display(self)
+
+class Methanocooccaes(Archaea):
+    def display(self)
+
+class Methanobacteriales(Archaea):
+    def display(self)
+
+class Methanomicrobiales(Archaea):
+    def display(self)
+
+class extreme_Halophiles(Archaea):
+    def display(self)
+    
+class Eucarya(Life):
+
+    def display(self)
+
+class Animalia(Eucarya):
+    def display(self)
+
+class Fungi(Eucarya):
+    def display(self)
+
+class Plantae(Eucarya):
+    def display(self)
+
+class Ciliates(Eucarya):
+    def display(self)
+
+class Flagellates(Eucarya):
+    def display(self)
+
+class Microsporidia(Eucarya):
+    def display(self)
