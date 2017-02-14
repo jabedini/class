b0c19a7f0b8bc5e6e03220b49518bcbcdbb984e1
jabedini <jaleh.amo@gmail.com>
2017-02-14 14:14:51 -0500

    Update gene_coverage_region.sql

--- BINF-6211/exam1/gene_coverage_region.sql
+++ BINF-6211/exam1/gene_coverage_region.sql
@@ -1,1 +1,4 @@
-SELECT gene_id, count(rs_id) FROM snps WHERE chromosome='14' AND position BETWEEN 30000000 AND 80000000 GROUP BY gene_id ORDER BY count(rs_id) DESC;
+SELECT gene_id, count(rs_id) FROM snps 
+WHERE chromosome='14' AND position 
+BETWEEN 30000000 AND 80000000 GROUP BY gene_id 
+ORDER BY count(rs_id) DESC;
