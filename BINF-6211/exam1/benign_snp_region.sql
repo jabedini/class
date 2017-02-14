0dee5139ddb05ed6ded549946fd07b5481fb2251
jabedini <jaleh.amo@gmail.com>
2017-02-14 14:14:28 -0500

    Update benign_snp_region.sql

--- BINF-6211/exam1/benign_snp_region.sql
+++ BINF-6211/exam1/benign_snp_region.sql
@@ -1,1 +1,3 @@
-SELECT rs_id FROM snps  WHERE chromosome='14' AND position BETWEEN 30000000 AND 80000000 AND clin_sig='Benign' ORDER BY position ASC;
+SELECT rs_id FROM snps  WHERE chromosome='14' AND position 
+BETWEEN 30000000 AND 80000000 AND clin_sig='Benign' 
+ORDER BY position ASC;
